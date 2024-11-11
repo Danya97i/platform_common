ce61b00a8de7cec1c9ca48e5c6790ba26a390e78
Danya97i <dan.deg97@vk.com>
2024-11-11 23:28:57 +0300

    pgx tx interface mock

--- pkg/db/generate.go
+++ pkg/db/generate.go
@@ -2,3 +2,4 @@ package db
 
 //go:generate sh -c "rm -rf mocks && mkdir -p mocks"
 //go:generate ../../bin/minimock -i Transactor -o ./mocks/ -s "_minimock.go"
+//go:generate ../../bin/minimock -i github.com/jackc/pgx/v4.Tx -o ./mocks/ -s "_minimock.go"
