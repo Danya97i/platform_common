08d4f52476a62325880a5de218e0598224637fc0
Danya97i <dan.deg97@vk.com>
2024-11-17 21:06:26 +0300

    TxManagerMock

--- pkg/db/generate.go
+++ pkg/db/generate.go
@@ -2,4 +2,5 @@ package db
 
 //go:generate sh -c "rm -rf mocks && mkdir -p mocks"
 //go:generate ../../bin/minimock -i Transactor -o ./mocks/ -s "_minimock.go"
+//go:generate ../../bin/minimock -i TxManager -o ./mocks/ -s "_minimock.go"
 //go:generate ../../bin/minimock -i github.com/jackc/pgx/v4.Tx -o ./mocks/ -s "_minimock.go"
